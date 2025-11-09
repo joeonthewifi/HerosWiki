ae45a273c5b14df57300ce54fd0963b3f6ecc5a6
Philip Huynh <wuflerz@outlook.com>
2025-11-09 02:46:00 +0200

    Fix icons

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,12 +1,31 @@
 import { docs } from '@/.source';
 import { type InferPageType, loader } from 'fumadocs-core/source';
-import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { createElement } from 'react';
+import { icons } from 'lucide-react';
+import Image from 'next/image';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
-  plugins: [lucideIconsPlugin()],
+  icon(icon) {
+    if (!icon) return;
+
+    if (icon.startsWith('/')) {
+      return createElement('div', {
+        className: 'md:-mt-0.5!',
+      }, createElement(Image, {
+        src: icon,
+        alt: '',
+        width: 24,
+        height: 24,
+      }));
+    }
+
+    if (icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
+    }
+  },
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
